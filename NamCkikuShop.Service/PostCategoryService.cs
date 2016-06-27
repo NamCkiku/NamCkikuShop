1306fceb02b75f97faf6c870210df705d877cca4
NamCkiku <tranhoangnam11373@gmail.com>
2016-06-27 15:20:54 +0700

    add API DEMO

---
+++ NamCkikuShop.Service/PostCategoryService.cs
@@ -0,0 +1,67 @@
+﻿using NamCkikuShop.Entities.Models;
+using NamCkikuShop.Repository.Infrastructure;
+using NamCkikuShop.Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamCkikuShop.Service
+{
+    public interface IPostCategoryService
+    {
+        void Add(PostCategory post);
+        void Update(PostCategory post);
+        void Delete(int id);
+        IEnumerable<PostCategory> GetAll();
+        IEnumerable<PostCategory> GetAllByParentID(int parentID);
+        PostCategory GetByID(int id);
+        void SaveChanges();
+    }
+    public class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        IUnitOfWork _IUnitOfWork;
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork IUnitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._IUnitOfWork = IUnitOfWork;
+        }
+
+        public void Add(PostCategory post)
+        {
+            _postCategoryRepository.Add(post);
+        }
+
+        public void Delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentID(int parentID)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentID);
+        }
+
+        public PostCategory GetByID(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void Update(PostCategory post)
+        {
+            _postCategoryRepository.Update(post);
+        }
+
+        public void SaveChanges()
+        {
+            _IUnitOfWork.Commit();
+        }
+    }
+}
---
+++ NamCkikuShop.Web/Api/PostCategoryController.cs
@@ -0,0 +1,48 @@
+﻿using NamCkikuShop.Entities.Models;
+using NamCkikuShop.Service;
+using NamCkikuShop.Web.Infrastructure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NamCkikuShop.Web.Api
+{
+    [RoutePrefix("api/postcategory")]
+    [Authorize]
+    public class PostCategoryController : ApiControllerBase
+    {
+        IPostCategoryService _postCategoryService;
+        public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService) : base(errorService)
+        {
+            this._postCategoryService = postCategoryService;
+
+        }
+        //[Route("add")]
+        //public HttpResponseMessage Post(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
+        //{
+        //    return CreateHttpResponse(request, () =>
+        //    {
+        //        HttpResponseMessage response = null;
+        //        if (ModelState.IsValid)
+        //        {
+        //            request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        //        }
+        //        else
+        //        {
+        //            PostCategory newPostCategory = new PostCategory();
+        //            newPostCategory.UpdatePostCategory(postCategoryVm);
+
+        //            var category = _postCategoryService.Add(newPostCategory);
+        //            _postCategoryService.Save();
+
+        //            response = request.CreateResponse(HttpStatusCode.Created, category);
+
+        //        }
+        //        return response;
+        //    });
+        }
+    }
+}
