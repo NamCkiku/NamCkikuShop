e51071934b763f0dcdf0af80588b2dacbf031830
NamCkiku <tranhoangnam11373@gmail.com>
2016-06-30 10:49:53 +0700

    add interface admin

---
+++ NamCkikuShop.Web/app/app.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="E:\_Study\_SourceCode\_SourseGithub\NamCkikuShop\NamCkikuShop.Web\Scripts/angular.js" />
+(function () {
+    angular.module("namckikuShop", []).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/homeView.html",
+            controller:"homeController",
+        });
+    }
+});
